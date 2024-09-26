dbe45c77641bb3fe0e71191ccc26cfbd5259423d
Elias Ainsworth <pilum-murialis@proton.me>
2024-09-26 13:11:14 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Kollektif Sans flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      # systems,
+      ...
+    }@inputs:
+    let
+      forEachSystem =
+        function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forEachSystem (pkgs: rec {
+        kollektif = pkgs.callPackage ./package.nix;
+        default = kollektif;
+      });
+    };
+}
